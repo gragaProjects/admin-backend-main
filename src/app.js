da39f0fa715f190c8220c6b10f8e3c392894b6fa
Sivakumar <balusiva2312@gmail.com>
2025-10-21 17:44:01 +0530

    New changes 1

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 // CORS
-app.use(cors());
+app.options('*', cors());
 
 // Request logging
 app.use(requestLogger);
